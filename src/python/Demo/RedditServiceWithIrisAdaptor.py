49f7a2c81c1f5c6c44ccede29530cc6a58f223b1
grongierisc <guillaume.rongier@intersystems.com>
2022-03-10 15:54:44 +0100

    reorder function

--- src/python/Demo/RedditServiceWithIrisAdaptor.py
+++ src/python/Demo/RedditServiceWithIrisAdaptor.py
@@ -3,13 +3,13 @@ import grongier.pex
 
 class RedditService(grongier.pex.BusinessService):
 
-    def OnProcessInput(self, messageInput):
-        msg = iris.cls("dc.Demo.PostMessage")._New()
-        msg.Post = messageInput
-        return self.SendRequestSync("FilterPosts",msg)
-
     def getAdapterType():
         """
         Name of the registred adaptor
         """
         return "dc.Reddit.InboundAdapter"
+
+    def OnProcessInput(self, messageInput):
+        msg = iris.cls("dc.Demo.PostMessage")._New()
+        msg.Post = messageInput
+        return self.SendRequestSync("FilterPosts",msg)
